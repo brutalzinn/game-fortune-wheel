8997c5509b37b70d17c2eb2e01a71f2606f0d546
Roberto Paes <robertocpaes@gmail.com>
2024-04-17 19:18:32 -0300

    wip: now is working

--- src/front/js/definitions.js
+++ src/front/js/definitions.js
@@ -5,7 +5,10 @@ const lblUserType = document.querySelector('#lbl_user_type')
 const gameList = document.getElementById('game_list');
 const lblWinner = document.querySelector('#lbl_winner')
 const spinEl = document.querySelector('#spin')
+const menu = document.querySelector('#menu')
 const clientId = crypto.randomUUID()
+const socket = io();
+let isOwner = false
 const events = {
     START: "START",
     CREATE: "CREATE",
--- src/front/js/menu.js
+++ src/front/js/menu.js
@@ -10,6 +10,19 @@ function init() {
        }
        refresh()
     })
+    shareBtn.addEventListener('click', async () => {
+        const shareData = {
+            title: "MDN",
+            text: "Learn web development on MDN!",
+            url: "https://developer.mozilla.org",
+          };
+        try {
+            await navigator.share(shareData);
+            resultPara.textContent = "MDN shared successfully";
+          } catch (err) {
+            resultPara.textContent = `Error: ${err}`;
+          }
+     })
   }
   init()
   
